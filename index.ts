030380c49c4313c7c2758ef61cd3c9a536bf2885
Patato <pathonscript@gmail.com>
2022-10-31 19:37:19 +0700

    forgor alphabet mb

--- index.ts
+++ index.ts
@@ -35,6 +35,7 @@ cunnier('mountainity')
 cunnier('NakZaa')
 cunnier('narze')
 cunnier('palmcm')
+cunnier('pathonscript')
 cunnier('Qwenty228')
 cunnier('rayriffy')
 cunnier('ronnapatp')
@@ -43,7 +44,6 @@ cunnier('sleepntsheep')
 cunnier('ST4RCHASER')
 cunnier('tinarskii')
 cunnier('WitMaster98')
-cunnier('pathonscript')
 
 //! Only edit the above lines and not the rest of this file.
 const coreMembers = team('core-members', {
